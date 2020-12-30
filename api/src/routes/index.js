5975ea917e6789d725af07e402840ffd716aa430
Dario Coronel <dariocoronel@MacBook-Pro-4.local>
2020-12-29 23:32:16 -0300

    adding multer for upload files

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,14 +1,20 @@
 import { Router } from 'express';
+import multer from 'multer';
 import UserController from '../app/controllers/UserController';
 import SessionController from '../app/controllers/SessionController';
 import Authmiddleware from '../app/middlewares/auth';
+import multerConfig from '../config/multer';
 
 const routes = new Router();
-
+const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/session', SessionController.store);
 
 routes.use(Authmiddleware);
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) =>
+  res.json({ ok: 'ok' })
+);
 
 export default routes;
