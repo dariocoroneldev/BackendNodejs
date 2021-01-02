5f1b47b0e50e4f49c30e4b69b6c1c23b75c2457d
Dario Coronel <dariocoronel@MacBook-Pro-4.local>
2021-01-02 15:33:38 -0300

    try to trate exceptions but fails with succes

--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,19 @@
 import express from 'express';
+import Youch from 'youch';
 import path from 'path';
+import * as Sentry from '@sentry/node';
+import 'express-async-errors';
 import routes from './routes';
+import sentryConfig from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentryConfig);
+    this.server.use(Sentry.Handlers.tracingHandler());
+    this.server.use(Sentry.Handlers.requestHandler());
     this.middleware();
     this.routes();
   }
@@ -21,6 +28,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exeptionHandler() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ api/src/config/sentry.js
@@ -0,0 +1,4 @@
+export default {
+  dsn:
+    'https://28f655b9429445408fb76cdbc4ef9764@o499069.ingest.sentry.io/5577209',
+};
